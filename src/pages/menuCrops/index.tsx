e690c16f4a2d86b6cb1619c4ca745757a761c390
MatheusCarvalho <matheusinholopescarvalho@gmail.com>
2022-03-14 16:22:10 -0300

    Ajustado Scroll MenuCrops

--- src/pages/menuCrops/index.tsx
+++ src/pages/menuCrops/index.tsx
@@ -1,47 +1,60 @@
 import React from 'react';
-import {useNav} from '../../utils/hooks';
+import {styles} from './styles';
 import * as Styles from './styles';
-import {FlatList, Image, ImageBackground, ScrollView} from 'react-native';
+import {useNav} from '../../utils/hooks';
+import IconButton from '../../components/IconButton';
 import CustomButton from '../../components/CustomButton';
-import {Text} from '../../components/Button/styles';
-import {string} from '../../languages';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+interface IButton {
+  icon: any;
+  onPress: Function;
+}
 
 const MenuCrop: React.FC = () => {
-  interface IButton {
-    icon: any;
-    onPress: Function;
-  }
   const navigation = useNav('menuCrops');
-
   const buttons: Array<IButton> = [
     {icon: require('../../assets/crops/barley.png'), onPress: () => {}},
+    {icon: require('../../assets/crops/olives.png'), onPress: () => {}},
+    {icon: require('../../assets/crops/cotton.png'), onPress: () => {}},
     {icon: require('../../assets/crops/wheat.png'), onPress: () => {}},
+    {icon: require('../../assets/crops/grapes.png'), onPress: () => {}},
+    {icon: require('../../assets/crops/grass.png'), onPress: () => {}},
+    {icon: require('../../assets/crops/barley.png'), onPress: () => {}},
+    {icon: require('../../assets/crops/olives.png'), onPress: () => {}},
+    {icon: require('../../assets/crops/cotton.png'), onPress: () => {}},
     {icon: require('../../assets/crops/wheat.png'), onPress: () => {}},
-    {icon: require('../../assets/crops/wheat.png'), onPress: () => {}},
-    {icon: require('../../assets/crops/wheat.png'), onPress: () => {}},
-    {icon: require('../../assets/crops/wheat.png'), onPress: () => {}},
+    {icon: require('../../assets/crops/grapes.png'), onPress: () => {}},
+    {icon: require('../../assets/crops/grass.png'), onPress: () => {}},
   ];
   return (
     <Styles.Container>
       <Styles.Header>
         <Styles.Title>Crops</Styles.Title>
       </Styles.Header>
-      <Styles.Body>
-        <ScrollView>
-          <Styles.List>
-            {buttons.map((v, i) => (
-              <CustomButton
-                onPress={v.onPress}
-                style={{...Styles.styles.btn}}
-                key={i}>
-                <Styles.ImageBg
-                  source={v.icon}
-                  style={{...Styles.styles.btnImage}}></Styles.ImageBg>
-              </CustomButton>
-            ))}
-          </Styles.List>
-        </ScrollView>
-      </Styles.Body>
+      <Styles.Scroll>
+        <Styles.Body>
+          {buttons.map((value, key) => (
+            <CustomButton
+              key={key}
+              onPress={value.onPress}
+              style={{...styles.btn}}>
+              <Styles.ImageBg
+                source={value.icon}
+                style={{...styles.btnImage}}
+              />
+            </CustomButton>
+          ))}
+        </Styles.Body>
+      </Styles.Scroll>
+      <Styles.BottomBox>
+        <IconButton
+          onPress={() => {
+            navigation.goBack();
+          }}
+          icon={<Ionicons name="arrow-back" style={styles.icon} />}
+        />
+      </Styles.BottomBox>
     </Styles.Container>
   );
 };
