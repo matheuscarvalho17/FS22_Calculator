7d09bd2704a6921bed9da9c486158607e8892f66
MatheusCarvalho <matheusinholopescarvalho@gmail.com>
2022-03-15 16:33:57 -0300

    Adicionado Types para react-native-navbar-color

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Main from '../pages/Main';
 import Crop from '../pages/Crop';
+import {useFlavor} from '../flavor';
 import Language from '../pages/Language';
 import MenuCrops from '../pages/MenuCrops';
 import Appearance from '../pages/Appearance';
+import NavigationBar from 'react-native-navbar-color';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import NavigationBar from 'react-native-navbar-color';
-import {useFlavor} from '../flavor';
+
 export type RootStackParamList = {
   mainPage: undefined;
   language: undefined;
@@ -19,6 +20,7 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 const App: React.FC = () => {
   const {colors} = useFlavor();
   NavigationBar.setColor(colors.primary);
+
   return (
     <NavigationContainer>
       <Stack.Navigator
