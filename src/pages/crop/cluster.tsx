77b025af6104fbd360c7fb977222f1e661479e0e
fukushiro <joao.castro@adaptersolucoes.com>
2022-03-29 12:07:02 -0300

    Crop - Criado subpaginas normal e cluster

---
+++ src/pages/crop/cluster.tsx
@@ -0,0 +1,151 @@
+import React, {useEffect, useMemo, useState} from 'react';
+import {View} from 'react-native';
+import {styles} from './styles';
+import * as Styles from './styles';
+import {useFlavor} from '../../flavor';
+import {useCrops} from '../../utils/database';
+import {useRoute} from '@react-navigation/native';
+import {useLanguage} from '../../languages';
+import {useNav} from '../../utils/hooks';
+import ComboBox, {Data} from '../../components/ComboBox';
+import CheckBox from '../../components/CheckBox';
+import TextInput from '../../components/TextInput';
+// import { Container } from './styles';
+interface INormal {
+  cropId: number;
+  setMeasureUnitLabel: Function;
+  unitsField: {
+    '(ha)': number;
+    '(acre)': number;
+    '(a)': number;
+    '(m²)': number;
+    '(ft²)': number;
+  };
+  targetHarvester: string;
+  setBonus: Function;
+  setRealBonus: Function;
+}
+const Cluster: React.FC<INormal> = ({
+  cropId,
+  setMeasureUnitLabel,
+  unitsField,
+  targetHarvester,
+  setBonus,
+  setRealBonus,
+}) => {
+  // !adubagem, rolagem, calagem
+  // !hooks
+  const crops = useCrops();
+  const route = useRoute();
+  const {colors} = useFlavor();
+  const {string} = useLanguage();
+  const navigation = useNav('crop');
+
+  // !useState
+
+  //const [bonus, setBonus] = useState<number>(0);
+  const [limed, setLimed] = useState<boolean>(false);
+  const [rolled, setRolled] = useState<boolean>(false);
+  const [plowed, setPlowed] = useState<boolean>(false);
+
+  const [mulched, setMulched] = useState<boolean>(false);
+
+  const [fertilized, setFertilized] = useState<Data>({
+    id: -1,
+    label: null,
+    value: null,
+  });
+  const [removedWeeds, setRemovedWeeds] = useState<Data>({
+    id: -1,
+    label: null,
+    value: null,
+  });
+
+  const [fertilizedItems] = useState<Array<Data>>(
+    useMemo(
+      () => [
+        {id: 0, label: string.no_fertilized, value: 'no_fertilized'},
+        {id: 1, label: string.half_fertilized, value: 'half_fertilized'},
+        {id: 2, label: string.full_fertilized, value: 'full_fertilized'},
+      ],
+      [],
+    ),
+  );
+  const [removedWeedItens] = useState<Array<Data>>(
+    useMemo(
+      () => [
+        {id: 0, label: string.no_removed_weeds, value: 'no_removed_weeds'},
+        {id: 1, label: string.mec_removed_weeds, value: 'mec_removed_weeds'},
+        {id: 2, label: string.chem_removed_weeds, value: 'chem_removed_weeds'},
+      ],
+      [],
+    ),
+  );
+
+  // !functions
+
+  function bonusCalculation() {
+    let multiplierByCare: number = 1;
+
+    if (plowed) {
+      multiplierByCare = multiplierByCare + 0.35;
+    }
+
+    if (fertilized.value === 'half_fertilized') {
+      multiplierByCare = multiplierByCare + 0.225;
+    }
+    if (fertilized.value === 'full_fertilized') {
+      multiplierByCare = multiplierByCare + 0.45;
+    }
+    if (removedWeeds.value === 'mec_removed_weeds') {
+      multiplierByCare = multiplierByCare + 0.2;
+    }
+    if (removedWeeds.value === 'chem_removed_weeds') {
+      multiplierByCare = multiplierByCare + 0.15;
+    }
+    setBonus((multiplierByCare - 1) * 100);
+    setRealBonus(multiplierByCare);
+    return multiplierByCare;
+  }
+
+  // !useEffect
+  useEffect(() => {
+    bonusCalculation();
+  }, [fertilized, removedWeeds, limed, plowed, rolled, mulched]);
+  return (
+    <View>
+      {/* //* Calculation method */}
+
+      {/* //* Field Care */}
+      <>
+        <Styles.SectionTitle colors={colors}>
+          {string.field_care}:
+        </Styles.SectionTitle>
+        <ComboBox
+          style={styles.margin5px}
+          value={fertilized}
+          data={fertilizedItems}
+          setValue={setFertilized}
+          placeholder={string.fertilized_stage}
+          modal_text={string.select_fertilized}
+        />
+        <ComboBox
+          style={styles.margin5px}
+          value={removedWeeds}
+          data={removedWeedItens}
+          setValue={setRemovedWeeds}
+          placeholder={string.weeds_stage}
+          modal_text={string.select_removed_weeds}
+        />
+        <CheckBox
+          value={plowed}
+          setValue={setPlowed}
+          style={styles.margin5px}
+          text={string.plowed_stage}
+        />
+      </>
+    </View>
+  );
+};
+
+export default Cluster;
--- src/pages/crop/index.tsx
+++ src/pages/crop/index.tsx
@@ -11,6 +11,9 @@ import IconButton from '../../components/IconButton';
 import ComboBox, {Data} from '../../components/ComboBox';
 import React, {useEffect, useMemo, useState} from 'react';
 import {roundNumber, onlyNumberAndDot} from '../../utils/masks';
+import Normal from './normal';
+import Cluster from './cluster';
+import {View} from 'react-native';
 
 const Crop: React.FC = ({}) => {
   const crops = useCrops();
@@ -19,55 +22,23 @@ const Crop: React.FC = ({}) => {
   const {cropId} = route.params;
   const {string} = useLanguage();
   const navigation = useNav('crop');
-  const [bonus, setBonus] = useState<number>(0);
-  const [limed, setLimed] = useState<boolean>(false);
-  const [rolled, setRolled] = useState<boolean>(false);
-  const [plowed, setPlowed] = useState<boolean>(false);
-  const [fieldSize, setFieldSize] = useState<string>('');
-  const [mulched, setMulched] = useState<boolean>(false);
-  const [yieldField, setYieldField] = useState<number>(0);
-  const [multiplier, setMultiplier] = useState<number>(1);
-  const [showWarn, setShowWarn] = useState<boolean>(false);
-  const [targetField, setTargetField] = useState<number>(0);
-  const [warnMessage, setWarnMessage] = useState<string>('');
+
+  // !useState
   const [mathBySize, setMathBySize] = useState<boolean>(true);
-  const [targetHarvester, setTargetHarvester] = useState<string>('');
+  const [showWarn, setShowWarn] = useState<boolean>(false);
+  const [warnMessage, setWarnMessage] = useState<string>('');
   const [measureUnitLabel, setMeasureUnitLabel] = useState<string>('');
-  const [fertilized, setFertilized] = useState<Data>({
-    id: -1,
-    label: null,
-    value: null,
-  });
-  const [removedWeeds, setRemovedWeeds] = useState<Data>({
-    id: -1,
-    label: null,
-    value: null,
-  });
+  const [bonusLabel, setBonusLabel] = useState<number>(0);
+  const [fieldSize, setFieldSize] = useState<string>('');
+  const [targetHarvester, setTargetHarvester] = useState<string>('');
+  const [bonus, setBonus] = useState<number>(0);
+  const [realBonus, setRealBonus] = useState<number>(0);
   const [measureUnit, setMeasureUnit] = useState<Data>({
     id: -1,
     label: null,
     value: null,
   });
-  const [fertilizedItems] = useState<Array<Data>>(
-    useMemo(
-      () => [
-        {id: 0, label: string.no_fertilized, value: 'no_fertilized'},
-        {id: 1, label: string.half_fertilized, value: 'half_fertilized'},
-        {id: 2, label: string.full_fertilized, value: 'full_fertilized'},
-      ],
-      [],
-    ),
-  );
-  const [removedWeedItens] = useState<Array<Data>>(
-    useMemo(
-      () => [
-        {id: 0, label: string.no_removed_weeds, value: 'no_removed_weeds'},
-        {id: 1, label: string.mec_removed_weeds, value: 'mec_removed_weeds'},
-        {id: 2, label: string.chem_removed_weeds, value: 'chem_removed_weeds'},
-      ],
-      [],
-    ),
-  );
+  const [multiplier, setMultiplier] = useState<number>(1);
   const [measureUnitItens] = useState<Array<Data>>(
     useMemo(
       () => [
@@ -93,9 +64,33 @@ const Crop: React.FC = ({}) => {
     '(m²)': 0.0001,
     '(ft²)': 0.0000092903,
   };
+  const [yieldField, setYieldField] = useState<number>(0);
+  const [targetField, setTargetField] = useState<number>(0);
+  // !function
+  function fieldCalculation(multiplierByCare: number) {
+    let fieldSize: number = 0;
 
+    fieldSize =
+      yieldField /
+      multiplierByCare /
+      parseFloat(targetHarvester) /
+      unitsField[measureUnit.value ? measureUnit.value : '(ha)'];
+
+    setTargetField(fieldSize);
+    setMeasureUnitLabel(measureUnit.value ? measureUnit.value : '(ha)');
+  }
   function handleChangeMathBySize() {
     setMathBySize(!mathBySize);
+  }
+  function yieldCalculation(multiplierByCare: number) {
+    let yieldMultiplier: number = 0;
+
+    yieldMultiplier =
+      parseFloat(fieldSize) *
+      multiplierByCare *
+      unitsField[measureUnit.value ? measureUnit.value : '(ha)'];
+
+    setMultiplier(yieldMultiplier);
   }
   function validateEntryData() {
     setShowWarn(false);
@@ -104,7 +99,7 @@ const Crop: React.FC = ({}) => {
     if (!mathBySize) {
       if (parseFloat(targetHarvester) > 0) {
         if (measureUnit.id != -1) {
-          let bonus: number = bonusCalculation();
+          let bonus: number = realBonus;
           fieldCalculation(bonus);
         } else {
           setWarnMessage(string.warn_measure_unit);
@@ -119,7 +114,7 @@ const Crop: React.FC = ({}) => {
     } else {
       if (parseFloat(fieldSize) > 0) {
         if (measureUnit.id != -1) {
-          let bonus: number = bonusCalculation();
+          let bonus: number = realBonus;
           yieldCalculation(bonus);
         } else {
           setWarnMessage(string.warn_measure_unit);
@@ -132,70 +127,20 @@ const Crop: React.FC = ({}) => {
         return;
       }
     }
+    setBonusLabel(bonus);
   }
-  function bonusCalculation() {
-    let multiplierByCare: number = 1;
-
-    if (limed) {
-      multiplierByCare = multiplierByCare + 0.15;
-    }
-    if (plowed) {
-      multiplierByCare = multiplierByCare + 0.15;
-    }
-    if (rolled) {
-      multiplierByCare = multiplierByCare + 0.025;
-    }
-    if (mulched) {
-      multiplierByCare = multiplierByCare + 0.025;
-    }
-    if (fertilized.value === 'half_fertilized') {
-      multiplierByCare = multiplierByCare + 0.225;
-    }
-    if (fertilized.value === 'full_fertilized') {
-      multiplierByCare = multiplierByCare + 0.45;
-    }
-    if (removedWeeds.value === 'mec_removed_weeds') {
-      multiplierByCare = multiplierByCare + 0.2;
-    }
-    if (removedWeeds.value === 'chem_removed_weeds') {
-      multiplierByCare = multiplierByCare + 0.15;
-    }
-    setBonus((multiplierByCare - 1) * 100);
-    return multiplierByCare;
-  }
-  function yieldCalculation(multiplierByCare: number) {
-    let yieldMultiplier: number = 0;
-
-    yieldMultiplier =
-      parseFloat(fieldSize) *
-      multiplierByCare *
-      unitsField[measureUnit.value ? measureUnit.value : '(ha)'];
-
-    setMultiplier(yieldMultiplier);
-  }
-  function fieldCalculation(multiplierByCare: number) {
-    let fieldSize: number = 0;
-
-    fieldSize =
-      yieldField /
-      multiplierByCare /
-      parseFloat(targetHarvester) /
-      unitsField[measureUnit.value ? measureUnit.value : '(ha)'];
-
-    setTargetField(fieldSize);
-    setMeasureUnitLabel(measureUnit.value ? measureUnit.value : '(ha)');
-  }
-
+  // !useEffect
   useEffect(() => {
     setYieldField(crops[cropId].yieldPerHa);
   }, []);
   useEffect(() => {
     setFieldSize('');
-    setShowWarn(false);
-    setWarnMessage('');
+    //setShowWarn(false);
+    //setWarnMessage('');
     setTargetHarvester('');
   }, [mathBySize]);
 
+  // !render
   return (
     <Styles.Container colors={colors}>
       <Styles.Header>
@@ -233,12 +178,11 @@ const Crop: React.FC = ({}) => {
           <Styles.DivideView>
             <Styles.TextInfos colors={colors}>{string.bonus}:</Styles.TextInfos>
             <Styles.TextInfosAccent colors={colors}>
-              +{roundNumber(bonus) + '%'}
+              +{roundNumber(bonusLabel) + '%'}
             </Styles.TextInfosAccent>
           </Styles.DivideView>
         </>
         <Styles.ScrollBody>
-          {/* //* Calculation method */}
           <>
             <Styles.SectionTitle colors={colors}>
               {string.calculation_method}:
@@ -288,52 +232,27 @@ const Crop: React.FC = ({}) => {
             />
             {showWarn && <Styles.warnMessage>{warnMessage}</Styles.warnMessage>}
           </>
-          {/* //* Field Care */}
-          <>
-            <Styles.SectionTitle colors={colors}>
-              {string.field_care}:
-            </Styles.SectionTitle>
-            <ComboBox
-              style={styles.margin5px}
-              value={fertilized}
-              data={fertilizedItems}
-              setValue={setFertilized}
-              placeholder={string.fertilized_stage}
-              modal_text={string.select_fertilized}
+
+          {crops[cropId].type == 'normal' && (
+            <Normal
+              cropId={cropId}
+              setBonus={setBonus}
+              setMeasureUnitLabel={setMeasureUnitLabel}
+              setRealBonus={setRealBonus}
+              targetHarvester={targetHarvester}
+              unitsField={unitsField}
             />
-            <ComboBox
-              style={styles.margin5px}
-              value={removedWeeds}
-              data={removedWeedItens}
-              setValue={setRemovedWeeds}
-              placeholder={string.weeds_stage}
-              modal_text={string.select_removed_weeds}
+          )}
+          {crops[cropId].type == 'cluster' && (
+            <Cluster
+              cropId={cropId}
+              setBonus={setBonus}
+              setMeasureUnitLabel={setMeasureUnitLabel}
+              setRealBonus={setRealBonus}
+              targetHarvester={targetHarvester}
+              unitsField={unitsField}
             />
-            <CheckBox
-              value={limed}
-              setValue={setLimed}
-              style={styles.margin5px}
-              text={string.limed_stage}
-            />
-            <CheckBox
-              value={plowed}
-              setValue={setPlowed}
-              style={styles.margin5px}
-              text={string.plowed_stage}
-            />
-            <CheckBox
-              value={rolled}
-              setValue={setRolled}
-              style={styles.margin5px}
-              text={string.rolled_stage}
-            />
-            <CheckBox
-              value={mulched}
-              setValue={setMulched}
-              style={styles.margin5px}
-              text={string.mulched_stage}
-            />
-          </>
+          )}
         </Styles.ScrollBody>
       </Styles.Body>
       <Styles.Footer>
